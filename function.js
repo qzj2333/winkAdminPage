cd436d6be18b19dae6cb35660e8a25a8107fcb2e
qzj2333 <l.wang12@wustl.edu>
2021-09-26 16:03:00 -0500

    Update function.js

--- function.js
+++ function.js
@@ -132,7 +132,7 @@ async function loadBugs()
         for(var i = 0; i < data.length; i++)
         {
             currData = data[i];
-            console.log("loadBug: "+currData._id.toString());
+            console.log("loadBug: "+currData._id.$oid);
             displayOneBugPost(currData._id, currData.username, currData.title, currData.detail, currData.rate);
         }
     } 
